2fcad3c5b8769d917361e27e0827555e581a29af
Alex <alemumu207@gmail.com>
2024-09-21 14:20:47 +0200

    Subiendo data folder

---
+++ src/data/db-images.js
@@ -0,0 +1,27 @@
+export const dbImages = [
+  {
+    id: 1,
+    name: 'Image1',
+    src: '/images/image-product-1.jpg',
+    srcThumbnail: '/images/image-product-1-thumbnail.jpg',
+  },
+  {
+    id: 2,
+    name: 'Image2',
+    src: '/images/image-product-2.jpg',
+    srcThumbnail: '/images/image-product-1-thumbnail.jpg',
+  },
+  {
+    id: 3,
+    name: 'Image3',
+    src: '/images/image-product-3.jpg',
+    srcThumbnail: '/images/image-product-1-thumbnail.jpg',
+  },
+  {
+    id: 4,
+    name: 'Image4',
+    src: '/images/image-product-4.jpg',
+    srcThumbnail: '/images/image-product-1-thumbnail.jpg',
+  },
+
+]
